22cc12c69290e508cf176d58b42a5c5d9fcb21c6
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-10-01 19:59:43 +0530

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { ChakraProvider, Container } from '@chakra-ui/react';
 import { theme } from './utils';
 import { Home } from './pages';
+import { Route, Routes } from 'react-router';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Container bgColor="#DDDDDD" centerContent minHeight="100vh" fontFamily="Helvetica">
-        <Home />
+      <Container
+        bgColor="#DDDDDD"
+        centerContent
+        minHeight="100vh"
+        fontFamily="Helvetica"
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </Container>
     </ChakraProvider>
   );
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <ColorModeScript />
-    <App />
+    <Router>
+      <ColorModeScript />
+      <App />
+    </Router>
   </StrictMode>
 );
