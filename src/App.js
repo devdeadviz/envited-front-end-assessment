034e954c76ba1e7732737d4d39a135d76026be3c
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-10-01 19:52:41 +0530

    feat: add landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { ChakraProvider, Box } from '@chakra-ui/react';
+import { ChakraProvider, Container } from '@chakra-ui/react';
 import { theme } from './utils';
+import { Home } from './pages';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center">
-      </Box>
+      <Container bgColor="#DDDDDD" centerContent minHeight="100vh" fontFamily="Helvetica">
+        <Home />
+      </Container>
     </ChakraProvider>
   );
 }
